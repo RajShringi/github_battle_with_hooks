77bb959e5108551c650c8f25fee0a73f1bfeda59
raj shringi <raj24shringi@gmail.com>
2022-12-28 19:20:13 +0530

    add Loader animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,17 @@ module.exports = {
     extend: {},
     animation: {
       jump: "jump .3s ease-in-out",
+      loader: "loader .8s ease-in-out infinite",
     },
     keyframes: {
       jump: {
         "0%": { transform: "scale(.3)" },
         "100%": { transform: "scale(1)" },
+      },
+      loader: {
+        "0%": { transform: "scale(.3)" },
+        "50%": { transform: "scale(1)" },
+        "100%": { transform: "scale(.3)" },
       },
     },
   },
