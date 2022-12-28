d6dfa244dc3b5b22f8a7ee1bc00755f2681f1f99
raj shringi <raj24shringi@gmail.com>
2022-12-28 19:21:20 +0530

    change animation to animate-loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,9 +1,9 @@
 function Loader() {
   return (
     <div className="flex justify-center items-center space-x-2">
-      <div className="h-3 w-3 bg-green-400 rounded-full animate-bounce"></div>
-      <div className="h-3 w-3 bg-green-400 rounded-full animate-bounce delay-700"></div>
-      <div className="h-3 w-3 bg-green-400 rounded-full animate-bounce"></div>
+      <div className="h-3 w-3 bg-green-400 rounded-full animate-loader"></div>
+      <div className="h-3 w-3 bg-green-400 rounded-full animate-loader"></div>
+      <div className="h-3 w-3 bg-green-400 rounded-full animate-loader"></div>
     </div>
   );
 }
